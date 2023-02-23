faa0ff17f6b67a232d075720ca29767aebe438e3
Hussein <aisackhussein@gmail.com>
2023-02-23 23:56:13 +0300

    Game logic

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -2,30 +2,47 @@
 
 const playerText = document.getElementById("playerText");
 const computerText = document.getElementById("computerText");
-const resultText = document.getElementById("resultText");
+const resultText = document.getElementById("result");
 
 const choiceBtns = document.querySelectorAll(".choiceBtn");
 let player;
 let computer;
 let result;
 
-choiceBtns.forEach((button) => button.addEventListener("click", () => ({
-  player = button.textContent
-  computerTurn()
-})));
+choiceBtns.forEach((button) =>
+  button.addEventListener("click", () => {
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner();
+  })
+);
 
-function   computerTurn(){
-  const randomNum = Math.floor(Math.random() * 3) + 1
+function computerTurn() {
+  const randomNum = Math.floor(Math.random() * 3) + 1;
 
-  switch(randomNum){
+  switch (randomNum) {
     case 1:
-      computer = 'Rock'
-      break
+      computer = "Rock";
+      break;
     case 2:
-      computer = 'Paper'
-      break
+      computer = "Paper";
+      break;
     case 3:
-      computer = 'Scissors'
-      break
+      computer = "Scissors";
+      break;
+  }
+}
+
+function checkWinner() {
+  if (player == computer) {
+    return "Draw!";
+  } else if (computer == "Rock") {
+    return player == "Paper" ? "You win" : "You lose!";
+  } else if (computer == "Paper") {
+    return player == "Scissors" ? "You win" : "You lose!";
+  } else if (computer == "Scissors") {
+    return player == "Rock" ? "You win" : "You lose!";
   }
 }
