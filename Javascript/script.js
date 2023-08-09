794b6019f2a919a9e22b50f7f69be8bdc4ac84ea
Hussein <aisackhussein@gmail.com>
2023-08-09 19:48:02 +0300

    Efficiency basics

--- Javascript/script.js
+++ Javascript/script.js
@@ -181,3 +181,16 @@ function Book() {
 function addBookToLibrary() {
   // do stuff here
 }
+
+// Efficiency basics
+function oddNumbersLessThanTen() {
+  let currentNumber = 1;
+
+  while (currentNumber < 10) {
+    if (currentNumber % 2 !== 0) {
+      console.log(currentNumber);
+    }
+
+    currentNumber += 1;
+  }
+}
