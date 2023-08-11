6323b4dee169f51fc269b71cac63aee5bfe7290c
Hussein <aisackhussein@gmail.com>
2023-08-11 23:15:28 +0300

    new obj

--- Javascript/test.js
+++ Javascript/test.js
@@ -53,3 +53,16 @@ const numOfClassesAttended = 0;
 if ((numOfClassesAttended / numOfClasses) * 100 > 75) {
   !attendance;
 }
+
+function Player(name, marker) {
+  this.name = name;
+  this.marker = marker;
+  this.sayName = function () {
+    console.log(name);
+  };
+}
+
+const player1 = new Player("steve", "X");
+const player2 = new Player("also steve", "O");
+player1.sayName(); // logs 'steve'
+player2.sayName(); // logs 'also steve'
