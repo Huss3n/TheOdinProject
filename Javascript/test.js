079762e45145a2c157e2708c9bc66237882edbac
Hussein <aisackhussein@gmail.com>
2023-08-18 23:55:37 +0300

    json

--- Javascript/test.js
+++ Javascript/test.js
@@ -75,3 +75,11 @@ function isGameOver() {
     DOMStuff.gameOver(this.winner);
   }
 }
+
+// json
+const myData = getData(); // if this is refactored to return a Promise...
+
+myData.then(function (data) {
+  // .then() tells it to wait until the promise is resolved
+  const pieceOfData = data["whatever"]; // and THEN run the function inside
+});
